862de3b7ae1fdbc25667e17a98a76bd7ed5d3814
Noah Esbold Zachrisson <noah.esboldzachrisson@elev.ga.ntig.se>
2026-03-11 13:45:34 +0100

    v4

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 let fade = document.querySelector(".fade-layer");
-fade.addEventListener("mouseover",showsearch)
-let Search = document.querySelector(".hover");
-menu.addEventListener("mouseover",showsearch)
+fade.addEventListener("click",showsearch)
+let outside = document.querySelector(".hover");
+outside.addEventListener("mouseover",showsearch)
 
 function showsearch(){
-    let Search = document.querySelector("body");
-    Search.classList.toggle("show");
+    let outside = document.querySelector("aside");
+    outside.classList.toggle("show");
 
     let fade = document.querySelector(".fade-layer");
     fade.classList.toggle("visible");
