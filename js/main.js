b633501bacf847911581673790aa1e4e053a03c1
noahez <noah.esboldzachrisson@elev.ga.ntig.se>
2026-04-19 22:57:32 +0200

    Final

--- js/main.js
+++ js/main.js
@@ -11,3 +11,11 @@ function showsearch(){
     fade.classList.toggle("visible");
 }
 
+document.getElementById("myBtn").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+function MyFunction(){
+  alert('Error')
+}
